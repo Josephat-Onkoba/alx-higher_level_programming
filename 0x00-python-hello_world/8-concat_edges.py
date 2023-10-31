88e5abbe138320edda2a3e37144c763e2629faff
Josephat-Onkoba <josephatonkoba30@gmail.com>
2023-10-30 23:48:27 -0700

    Complete this source code to print object-oriented programming with Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + " " + str[107:111] + " " + str[0:6]
+
 print(str)
