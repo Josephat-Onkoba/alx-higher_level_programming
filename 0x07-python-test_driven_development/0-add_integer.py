b9a34d828a50624cb199762db4647d875149e741
Josephat-Onkoba <josephatonkoba30@gmail.com>
2024-04-06 02:22:10 -0700

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+        int: The addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+# Test cases
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
